5b605015d19016cddb8e9fd0fffb10f432244c11
RAFAELLA MARIA TOSCANO REIS <24122319@dsn127m1249387.cfp127aluno.local>
2025-03-11 12:21:47 -0300

    user data screen finalizado

--- app/src/main/java/br/senai/sp/jandira/bmi/screens/TelaInicial.kt
+++ app/src/main/java/br/senai/sp/jandira/bmi/screens/TelaInicial.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
@@ -20,7 +19,6 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -28,10 +26,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.VerticalAlignmentLine
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -40,24 +35,37 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
 import br.senai.sp.jandira.bmi.R
 
 @Composable
-fun TelaInicial(modifier: Modifier = Modifier) {
+fun TelaInicial(navController: NavHostController?) {
 
     var nomeState = remember {
         mutableStateOf(value = "")
     }
 
+    var isErrorState = remember {
+        mutableStateOf(value = false)
+    }
+
+    var errorMessageState = remember {
+        mutableStateOf(value = "")
+    }
+
+    var context = LocalContext.current
+
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(brush = Brush.linearGradient(
-                listOf(
-                    Color(0xFFA5EEC3),
-                    Color(0xFFE7E528)
+            .background(
+                brush = Brush.linearGradient(
+                    listOf(
+                        Color(0xFFA5EEC3),
+                        Color(0xFFE7E528)
+                    )
                 )
-            )),
+            ),
         contentAlignment = Alignment.BottomCenter
     ) {
         Column(
@@ -83,58 +91,71 @@ fun TelaInicial(modifier: Modifier = Modifier) {
                     .height(450.dp),
                 shape = RoundedCornerShape(
                     topStart = 50.dp,
-                    topEnd = 50.dp)
-            ) {
-            Column(
-                verticalArrangement = Arrangement.SpaceBetween,
-                horizontalAlignment = Alignment.End,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(32.dp)
-            ) {
-                Column {
-                    Text(
-                    text = stringResource(R.string.your_name),
-                    fontSize = 22.sp,
-                    fontWeight = FontWeight.Bold
+                    topEnd = 50.dp
                 )
-                    OutlinedTextField(
-                        value = nomeState.value,
-                        onValueChange = { it ->
-                            nomeState.value = it
-                        },
-
-                        modifier =  Modifier
-                            .fillMaxWidth()
-                            .padding(top = 10.dp),
-                        label = {Text( stringResource(R.string.your_name_here))},
-                        trailingIcon = {
-                            Icon(
-                                imageVector = Icons.Default.PersonPin,
-                                contentDescription = "",
-                                tint = Color.Black
-                            )
-                        },
-                        keyboardOptions = KeyboardOptions(
-                            keyboardType = KeyboardType.Text,
-                            capitalization = KeyboardCapitalization.Words
-                        )
-
-                    )
-                }
-                Button(
-                    onClick = {},
-
+            ) {
+                Column(
+                    verticalArrangement = Arrangement.SpaceBetween,
+                    horizontalAlignment = Alignment.End,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(32.dp)
                 ) {
-                    Text(
-                        text = stringResource(R.string.next)
-                    )
-                    Icon(
-                        imageVector = Icons.Filled.ArrowForward,
-                        contentDescription = ""
-                    )
+                    Column {
+                        Text(
+                            text = stringResource(R.string.your_name),
+                            fontSize = 22.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                        OutlinedTextField(
+                            value = nomeState.value,
+                            onValueChange = { it ->
+                                nomeState.value = it
+                            },
+
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 10.dp),
+                            label = { Text(stringResource(R.string.your_name_here)) },
+                            trailingIcon = {
+                                Icon(
+                                    imageVector = Icons.Default.PersonPin,
+                                    contentDescription = "",
+                                    tint = Color.Black
+                                )
+                            },
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Text,
+                                capitalization = KeyboardCapitalization.Words
+                            ),
+                            isError = isErrorState.value,
+                            supportingText = {
+                                Text(
+                                    text = errorMessageState.value,
+                                    color = Color(0xFF4B6B26)
+                                )
+                            }
+                        )
+                    }
+                    Button(
+                        onClick = {
+                            if (nomeState.value.length < 3) {
+                                isErrorState.value = true
+                                errorMessageState.value = context.getString(R.string.support_name)
+                            } else {
+                                navController?.navigate("user_data")
+                            }
+                        },
+                    ) {
+                        Text(
+                            text = stringResource(R.string.next)
+                        )
+                        Icon(
+                            imageVector = Icons.Filled.ArrowForward,
+                            contentDescription = ""
+                        )
+                    }
                 }
-            }
             }
         }
     }
@@ -143,5 +164,5 @@ fun TelaInicial(modifier: Modifier = Modifier) {
 @Preview(showSystemUi = true)
 @Composable
 private fun TelaInicialPreview() {
-    TelaInicial()
+    TelaInicial(null)
 }
